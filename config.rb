f9c01cf13d35cbef1164dc2fd20e0bc1ed925789
Ajmal Eylia <aj.eylia@gmail.com>
2012-01-18 17:15:00 +0800

    gemfile, readme, config.rb, change img dir

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
@@ -13,8 +13,8 @@ javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
+output_style = :compressed
 line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
---
+++ readme.md
@@ -0,0 +1,14 @@
+#Jekyll framework.
+
+This is a blank jekyll framework created for rapid html conversion.
+
+credit: mojombo and Eric D. Field
+
+##config.rb for css
+output_style = :compressed
+line_comments = false
+
+##How to use
+1. cd dir
+2. jekyll --server --auto
+3. compass watch
